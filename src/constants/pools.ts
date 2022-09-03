2454eaeeb41db5d36c1c827411cacf9a2d52822b
puginufinance <112399794+puginufinance@users.noreply.github.com>
2022-09-03 08:10:01 +0700

    Update pools.ts

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,7 +1,7 @@
 export const Pools = [
     {
         poolAddresses: {
-            97: '0x82E8a3D5D58a4c863c92f4aFA8FfFC663CB97020',
+            97: '0x16F98c775592341d8365dD3C3e0a1318D44DE3bF',
         },
         stakingTokenAddresses: {
             97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
@@ -15,7 +15,7 @@ export const Pools = [
     },
     {
         poolAddresses: {
-            97: '0xDE7F236a23486Bc9F0aA7567adC1a8b0cA203B4d',
+            97: '0x6BF89974B3CD6BE16e9A294C94F44140cb0F659d',
         },
         stakingTokenAddresses: {
             97: '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee',
@@ -29,10 +29,10 @@ export const Pools = [
     },
     {
         poolAddresses: {
-            97: '0xff201A166D7222E31FA438550b44bB8a223311dF',
+            97: '0x66B3B8719F3794Ea69713ACfCc750f27A0bAbCb2',
         },
         stakingTokenAddresses: {
-            97: '0x8105139F0Be871262C342eEA1FD0011181209217',
+            97: '0x47161F942087eab16292d02d5d68EE26C41a364b',
         },
         name: 'BIC',
         symbol: 'BIC',
@@ -40,6 +40,48 @@ export const Pools = [
         isWBNB: false,
         icon: 'bic',
         magnification: 3,
+    },
+    {
+        poolAddresses: {
+            97: '0x8AC8519822b0093B886B5586c8ddF36218E31A13',
+        },
+        stakingTokenAddresses: {
+            97: '0x64544969ed7EBf5f083679233325356EbE738930',
+        },
+        name: 'USDC',
+        symbol: 'USDC',
+        tokenSymbol: 'USDC',
+        isWBNB: false,
+        icon: 'usdc',
+        magnification: 1,
+    },
+    {
+        poolAddresses: {
+            97: '0x81C7AA290ddC42eBfEa809e7627A64e7ae5b3D19',
+        },
+        stakingTokenAddresses: {
+            97: '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd',
+        },
+        name: 'USDT',
+        symbol: 'USDT',
+        tokenSymbol: 'USDT',
+        isWBNB: false,
+        icon: 'usdt',
+        magnification: 1,
+    },
+    {
+        poolAddresses: {
+            97: '0x959f9ae2Ca66730e6e451a10aE64D3EF0F15e0D0',
+        },
+        stakingTokenAddresses: {
+            97: '0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867',
+        },
+        name: 'DAI',
+        symbol: 'DAI',
+        tokenSymbol: 'DAI',
+        isWBNB: false,
+        icon: 'dai',
+        magnification: 1,
 
     },
 ]
