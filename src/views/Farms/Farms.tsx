efa7153b7a19f9fe3f810a47552c6ab968d286d3
aonhac <105034482+aonhac@users.noreply.github.com>
2022-09-03 11:16:48 +0700

    Update Farms.tsx

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -23,7 +23,7 @@ const Farms: React.FC = () => {
         {account ? (
           <>
             <Route exact path={path}>
-              <PageHeader title="Stake tokens to stack BIC" subtitle="PUG PRICE: $0.000088" />
+              <PageHeader title="Stake tokens to stack BIC" subtitle="BIC PRICE: $0.0.000000528465" />
               <FarmCards />
             </Route>
             <Route path={`${path}/:farmId`}>
